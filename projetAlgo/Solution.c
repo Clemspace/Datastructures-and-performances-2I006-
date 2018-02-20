36b51fa78e4d83e85da25c0c15150d543d02aef4
Clemspace <36642506+Clemspace@users.noreply.github.com>
2018-02-20 14:35:34 +0100

    Add files via upload

---
+++ projetAlgo/Solution.c
@@ -0,0 +1,119 @@
+#include "Solution.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include"entree_sortie.h"
+#include<string.h>
+
+void Solution_init(Solution *S){
+
+  S->prem=NULL;
+  S->dern=NULL;
+  S->cptr_pas=0;  
+}
+
+void Ajout_action(Solution *S, char a){
+  Cell_char *nouv=(Cell_char*) malloc(sizeof(Cell_char));
+  nouv->a=a;
+  nouv->suiv=NULL;
+  if (S->prem==NULL){  // Si Liste vide
+    S->prem=nouv;
+    S->dern=nouv;
+  }
+  else{
+    S->dern->suiv=nouv;
+    S->dern=nouv;
+  }
+  if (a!='S') S->cptr_pas++;
+}
+
+
+void Affiche(Solution *S){
+  Cell_char *cour=S->prem;
+  printf("Nombre de pas: %d\n",S->cptr_pas);
+  printf("Section jouee: ");
+  while (cour!=NULL){
+    printf("%c ",cour->a);
+    cour=cour->suiv;
+  }
+  printf("\n");
+}
+
+void Ecriture_Disque(int m, int n, int nbcoul, int graine, Solution *S){
+  char filename[104];
+  char inttochar[6];
+  
+  strcpy(filename,"Robot_");
+  sprintf(inttochar,"%d",m);
+  strcat(filename,inttochar);
+  strcat(filename,"_");
+  sprintf(inttochar,"%d",n);
+  strcat(filename,inttochar);
+  strcat(filename,"_");
+  sprintf(inttochar,"%d",nbcoul);
+  strcat(filename,inttochar);
+  strcat(filename,"_");
+  sprintf(inttochar,"%d",graine);
+  strcat(filename,inttochar);
+  strcat(filename,".sol");
+
+  FILE *f=fopen(filename,"w");
+  fprintf(f,"%d %d %d %d\n", m, n, nbcoul, graine);
+
+  Cell_char *cour=S->prem;
+  while (cour!=NULL){
+    fprintf(f,"%c ",cour->a);
+    cour=cour->suiv;
+  }
+  fprintf(f,"0\n");
+
+  fclose(f);
+}
+
+void Lecture_Disque(char *filename, int *m, int *n, int *nbcoul, int *graine, Solution *S){
+  char c;
+  FILE* f=fopen(filename,"r");
+  *m=GetEntier(f);
+  *n=GetEntier(f);
+  *nbcoul=GetEntier(f);
+  *graine=GetEntier(f);
+  
+  c=ReadChar(f);
+  while (c!='0'){
+    Ajout_action(S,c);
+    c=ReadChar(f);
+  }
+  
+  fclose(f);  
+}
+
+void PlusCourtChemin(Solution *S, int i, int j, int k,int l){
+
+  if(k-i>0){ //si la destination est a droite
+
+    Ajout_action(S, 'R');
+    return PlusCourtChemin(S,i+1,j,k,l);
+
+
+  }
+  else if(k-i<0){//si la destination est à gauche
+
+    Ajout_action(S,'L');
+    return PlusCourtChemin(S,i-1,j,k,l);
+
+  } 
+  else if(l-j>0){// si la destination est en bas
+        Ajout_action(S,'D');
+        return PlusCourtChemin(S,i,j-1,k,l);
+
+
+  }
+  else if(l-j<0){ //si la destination est en haut
+      Ajout_action(S,'U');
+      return PlusCourtChemin(S,i,j+1,k,l);
+
+
+  }
+  else return; //si on est arrivés
+
+}
+
